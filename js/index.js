f5901c55aad2d25114e10c47b359b5cad5e97257
Michael Heimrath <mheimrath1994@gmail.com>
2021-01-24 22:58:54 +0100

    almost works as it should

--- js/index.js
+++ js/index.js
@@ -27,8 +27,10 @@ const cards = [
 
 const memoryGame = new MemoryGame(cards);
 
+memoryGame.shuffleCards();
 window.addEventListener('load', event => {
   let html = '';
+
   memoryGame.cards.forEach(pic => {
     html += `<div class="card" data-card-name="${pic.name}">`;
     html += `<div class="back" name="${pic.img}"></div>`;
@@ -40,10 +42,37 @@ window.addEventListener('load', event => {
   document.querySelector('#memory-board').innerHTML = html;
 
   // Bind the click event of each element to a function
+  
   document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('click', () => {
+        card.classList.toggle('turned')
+        memoryGame.pickedCards.push(card);
+        console.log(memoryGame.pickedCards);
+        if (memoryGame.pickedCards.length === 3) {
+          console.log(memoryGame.pickedCards[0].innerText)
+          if (memoryGame.checkIfPair(memoryGame.pickedCards[0].innerHTML, memoryGame.pickedCards[1].innerHTML)) {
+            memoryGame.pickedCards[0].classList.add('blocked')
+            memoryGame.pickedCards[1].classList.add('blocked')
+            memoryGame.pickedCards[2].classList.remove('turned')
+          }
+          else {
+            memoryGame.pickedCards[0].classList.remove('turned')
+            memoryGame.pickedCards[1].classList.remove('turned')
+            memoryGame.pickedCards[2].classList.remove('turned')
+          }
+          
+          memoryGame.pickedCards = [];
+          if (  memoryGame.isFinished()) {
+            console.log('you win')
+          }
+        }              
       // TODO: write some code here
       console.log(`Card clicked: ${card}`);
     });
+    
+
   });
+
+
+
 });
